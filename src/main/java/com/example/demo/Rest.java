9fcf1375e72e749b582e8cd42fc92b097546a7c5
Fábián Levente <97455713+fablevi@users.noreply.github.com>
2024-01-14 19:52:44 +0100

    Update

--- src/main/java/com/example/demo/Rest.java
+++ src/main/java/com/example/demo/Rest.java
@@ -2,12 +2,17 @@ package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
 public class Rest {
 	
+	 private static final Logger logger = LoggerFactory.getLogger(Rest.class);
+	
 	@GetMapping("/hw")
 	String hw() {
+		logger.info("Hello world!!!");
 		return "Hello world";
 	}
 }
